d5ab628c62ee0ed915e5ec24bf959dadcf1c73f8
mxinburritos <mxinburritos@gmail.com>
2021-08-11 16:08:27 -0700

    Fix unmerged files

--- Webpack/src/index.js
+++ Webpack/src/index.js
@@ -6,14 +6,6 @@ const log = (msg) => {
     document.body.appendChild(elem)
 }
 
-<<<<<<< HEAD
-// Create the client with default options
-const client = new StreamrClient()
-
-document.getElementById('subscribe').addEventListener('click', () => {
-    // Subscribe to a stream
-    const subscription = client.subscribe({
-=======
 // REPLACE THIS WITH YOUR OWN PRIVATE KEY
 const PRIVATE_KEY = 'MY-PRIVATE-KEY'
 
@@ -38,7 +30,6 @@ document.getElementById('subscribe').addEventListener('click', async () => {
 
     // Subscribe to a stream
     await client.subscribe({
->>>>>>> update-examples-from-monorepo
         stream: '7wa7APtlTq6EC5iTCBy6dw',
         // Resend the last 10 messages on connect
         resend: {
@@ -53,27 +44,6 @@ document.getElementById('subscribe').addEventListener('click', async () => {
     client.on('connected', () => {
         log('A connection has been established!')
     })
-<<<<<<< HEAD
-
-    subscription.on('subscribed', () => {
-        log(`Subscribed to ${subscription.streamId}`)
-    })
-
-    subscription.on('resending', () => {
-        log(`Resending from ${subscription.streamId}`)
-    })
-
-    subscription.on('resent', () => {
-        log(`Resend complete for ${subscription.streamId}`)
-    })
-
-    subscription.on('no_resend', () => {
-        log(`Nothing to resend for ${subscription.streamId}`)
-    })
-})
-
-document.getElementById('publish').addEventListener('click', () => {
-=======
 })
 
 document.getElementById('publish').addEventListener('click', async () => {
@@ -82,19 +52,12 @@ document.getElementById('publish').addEventListener('click', async () => {
         return;
     }
 
->>>>>>> update-examples-from-monorepo
     // Here is the event we'll be sending
     const msg = {
         hello: 'world',
         random: Math.random(),
     }
 
-<<<<<<< HEAD
-    // Publish the event to the Stream
-    client.publish('MY-STREAM-ID', msg, 'MY-API-KEY')
-        .then(() => log('Sent successfully: ', msg))
-        .catch((err) => log(err))
-=======
     client.getOrCreateStream({
         id: `${await client.getAddress()}/webpack-example-data`,
     }).then((stream) => setInterval(() => {
@@ -103,5 +66,4 @@ document.getElementById('publish').addEventListener('click', async () => {
             .then(() => log(`Sent successfully: ${JSON.stringify(msg)}`))
             .catch((err) => console.error(err))
     }, 1000))
->>>>>>> update-examples-from-monorepo
 })
